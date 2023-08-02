3ff001df0855efe764773f45869f888b33d237e3
jinyulin <yulin.jin@chiplego.com>
2023-08-02 14:09:21 +0800

    update

--- lvim/config.lua
+++ lvim/config.lua
@@ -86,6 +86,9 @@ normal_map["ma"] = ":Telescope vim_bookmarks current_file<cr>"
 normal_map["mA"] = ":Telescope vim_bookmarks all<cr>"
 normal_map["H"] = "^"
 normal_map["L"] = "$"
+normal_map["[d"] = ":Lspsaga diagnostic_jump_prev<CR>"
+normal_map["]d"] = ":Lspsaga diagnostic_jump_next<CR>"
+normal_map["<M-o>"] = ":ClangdSwitchSourceHeader<CR>"
 
 
 local visual_map = lvim.keys.visual_mode
@@ -151,7 +154,23 @@ which_map['d'] = {
       enter = true,
       position = "center",
     })
-  end, "Debug colsole" }
+  end, "Debug colsole" },
+  t = { function()
+    require("dapui").float_element("console", {
+      width = math.floor(vim.api.nvim_win_get_width(0) * 1),
+      height = math.floor(vim.api.nvim_win_get_height(0) * 1),
+      enter = true,
+      position = "center",
+    })
+  end, "Debug colsole" },
+  b = { "<cmd>Trouble document_diagnostics<CR>", "Buffer diagnostics" },
+  w = { "<cmd>Trouble workspace_diagnostics<CR>", "Workspace diagnostics" },
+}
+
+which_map['c'] = {
+  name = 'code',
+  a = { ":Lspsaga code_action<CR>", "Code action" },
+  f = { "<cmd>lua vim.lsp.buf.format()<CR>", "Code format" },
 }
 
 -- lvim.lsp.buffer_mappings.normal_mode.
@@ -178,6 +197,69 @@ lvim.builtin.lualine.sections.lualine_c = { 'lsp_progress' }
 -- lvim.builtin.indentlines.active = false
 
 lvim.plugins = {
+  {
+    "folke/trouble.nvim",
+    dependecies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+
+    },
+    config = function()
+      require 'trouble'.setup({
+        position = "bottom", -- position of the list can be: bottom, top, left, right
+        height = 10, -- height of the trouble list when position is top or bottom
+        width = 50, -- width of the list when position is left or right
+        icons = true, -- use devicons for filenames
+        mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+        severity = nil, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
+        fold_open = "", -- icon used for open folds
+        fold_closed = "", -- icon used for closed folds
+        group = true, -- group results by file
+        padding = true, -- add an extra new line on top of the list
+        cycle_results = true, -- cycle item list when reaching beginning or end of list
+        action_keys = { -- key mappings for actions in the trouble list
+          -- map to {} to remove a mapping, for example:
+          -- close = {},
+          close = "q",                                                                        -- close the list
+          cancel = "<esc>",                                                                   -- cancel the preview and get back to your last window / buffer / cursor
+          refresh = "r",                                                                      -- manually refresh
+          jump = { "<cr>", "<tab>", "<2-leftmouse>" },                                        -- jump to the diagnostic or open / close folds
+          open_split = { "<c-x>" },                                                           -- open buffer in new split
+          open_vsplit = { "<c-v>" },                                                          -- open buffer in new vsplit
+          open_tab = { "<c-t>" },                                                             -- open buffer in new tab
+          jump_close = { "o" },                                                               -- jump to the diagnostic and close the list
+          toggle_mode = "m",                                                                  -- toggle between "workspace" and "document" diagnostics mode
+          switch_severity = "s",                                                              -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
+          toggle_preview = "P",                                                               -- toggle auto_preview
+          hover = "K",                                                                        -- opens a small popup with the full multiline message
+          preview = "p",                                                                      -- preview the diagnostic location
+          open_code_href = "c",                                                               -- if present, open a URI with more information about the diagnostic error
+          close_folds = { "zM", "zm" },                                                       -- close all folds
+          open_folds = { "zR", "zr" },                                                        -- open all folds
+          toggle_fold = { "zA", "za" },                                                       -- toggle fold of current file
+          previous = "k",                                                                     -- previous item
+          next = "j",                                                                         -- next item
+          help = "?",                                                                         -- help menu
+        },
+        multiline = true,                                                                     -- render multi-line messages
+        indent_lines = true,                                                                  -- add an indent guide below the fold icons
+        auto_open = false,                                                                    -- automatically open the list when you have diagnostics
+        auto_close = false,                                                                   -- automatically close the list when you have no diagnostics
+        auto_preview = true,                                                                  -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+        auto_fold = false,                                                                    -- automatically fold a file trouble list at creation
+        auto_jump = { "lsp_definitions" },                                                    -- for the given modes, automatically jump if there is only a single result
+        include_declaration = { "lsp_references", "lsp_implementations", "lsp_definitions" }, -- for the given modes, include the declaration of the current symbol in the results
+        signs = {
+          -- icons / text used for a diagnostic
+          error = "",
+          warning = "",
+          hint = "",
+          information = "",
+          other = "",
+        },
+        use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+      })
+    end
+  },
   {
     "voldikss/vim-translator",
     config = function()
@@ -503,12 +585,13 @@ lvim.builtin.dap.ui.config.layouts = {
   {
     elements = {
       -- Elements can be strings or table with id and size keys.
-      { id = "scopes",      size = 0.3 },
-      { id = "watches",     size = 0.25 },
-      { id = "stacks",      size = 0.3 },
-      { id = "breakpoints", size = 0.15 },
+      { id = "repl",   size = 0.7 },
+      { id = "scopes", size = 0.3 },
+      -- { id = "watches",     size = 0.3 },
+      -- { id = "stacks",      size = 0.3 },
+      -- { id = "breakpoints", size = 0.15 },
     },
-    size = 40, -- 40 columns
+    size = 30, -- 40 columns
     position = "left",
   },
   {
@@ -529,7 +612,7 @@ dap.adapters.codelldb = {
   host = '127.0.0.1',
   port = 13123,
   executable = {
-    command = '/home/jyl/.local/share/lvim/mason/bin/codelldb',
+    command = '/home/jinyulin/.local/share/lvim/mason/bin/codelldb',
     args = { "--port", "13123" },
   },
 }
